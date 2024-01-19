06206290f467b2ce9821b1dc079caf92b8913658
vidigg <119675844+vidiggwp@users.noreply.github.com>
2024-01-19 18:31:46 +0700

    Fix Jadwal Kamis

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -35,7 +35,15 @@ const Kamis = () => {
 				data-aos="fade-up"
 				data-aos-duration="1000">
 				<div className="w-[50%] text-base font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">12.30 - 15.00</div>
+				<div className="flex justify-center items-center text-sm">11.45 - 12.30</div>
+			</div>
+
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">Matematika</div>
+				<div className="flex justify-center items-center text-sm">12.30 - 14.30</div>
 			</div>
 		</>
 	)
