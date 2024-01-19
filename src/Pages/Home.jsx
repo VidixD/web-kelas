c3c39e6095c708cf6cd79f626127e341387581d0
vidigg <119675844+vidiggwp@users.noreply.github.com>
2024-01-19 17:40:36 +0700

    Changed Angka Gradient

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,18 +13,21 @@ const Home = () => {
 					className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative"
 					id="TotalSiswa">
 					<div id="AngkaGradientBlue" className="flex items-center justify-center ">
-						25
+						XI
 					</div>
 					<div className="">
 						<span id="AngkaGradientBlue" className="text-5xl">
-							3
+							R
 						</span>
 						<span id="AngkaGradientPink" className="text-5xl">
-							7
+							P
+						</span>
+						<span id="AngkaGradientBlue" className="text-5xl">
+							L
 						</span>
 					</div>
 					<div id="AngkaGradientPink" className="flex items-center justify-center">
-						12
+						1
 					</div>
 				</div>
 
