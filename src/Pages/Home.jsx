296b9d2cb4269c3df89f9565577e938cbbf0057d
vidigg <119675844+vidiggwp@users.noreply.github.com>
2024-01-19 17:49:17 +0700

    HAHA

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -13,7 +13,10 @@ const Home = () => {
 					className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative"
 					id="TotalSiswa">
 					<div id="AngkaGradientBlue" className="flex items-center justify-center ">
-						XI
+						X
+					</div>
+					<div id="AngkaGradientIjo" className="flex items-center justify-center ">
+						I
 					</div>
 					<div className="">
 						<span id="AngkaGradientBlue" className="text-5xl">
